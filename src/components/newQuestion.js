3caaee2bae2775e57b044baa0801eb48def0a336
smaruns3nthil <smarunsenthil@gmail.com>
2021-01-08 18:55:10 -0500

    Adding New Question to the store

--- src/components/newQuestion.js
+++ src/components/newQuestion.js
@@ -1,13 +1,68 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-
+import {handleAddQuestion} from '../actions/question'
 
 class Add extends Component {
-  
+  state ={
+    optionOne:'',
+    optionTwo:''
+  }
+  handleOptionOne = (e) => {
+    const optionOne = e.target.value
+
+    this.setState(() => ({
+      optionOne
+    }))
+  }
+  handleOptionTwo = (e) => {
+    const optionTwo = e.target.value
+
+    this.setState(() => ({
+      optionTwo
+    }))
+  }
+  handleSubmit = (e) => {
+    e.preventDefault()
+
+    const { optionOne,optionTwo } = this.state
+    const {dispatch}=this.props
+
+    dispatch(handleAddQuestion(
+      optionOne,
+      optionTwo,
+    ))
+    this.setState(() => ({
+      optionOne: '',
+      optionTwo: ''
+
+    }))
+  }
+
   render() {
+    const {optionOne,optionTwo}=this.state
     return (
       <div>
-        New Question
+         <h3>Create New Question</h3>
+         <h3>Would you Rather??</h3>
+         <form className='new-tweet' onSubmit={this.handleSubmit}>
+          <textarea
+            placeholder="Option 1"
+            value={optionOne}
+            onChange={this.handleOptionOne}
+          />
+          <br/>OR<br/>
+          <textarea
+            placeholder="Option 2"
+            value={optionTwo}
+            onChange={this.handleOptionTwo}
+          />
+          <br/>
+          <button
+            type='submit'
+            disabled={ optionOne=== '' && optionTwo === ''}>
+              Submit
+          </button>
+        </form>
       </div>
     )
   }
